6456517a2104baeae042e2b3d1cb9121f9728be9
eriker75 <15-06637@usb.ve>
2021-07-26 18:29:28 -0400

    clean react installation

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+    return(
+        <>
+            <h1>Hola React</h1>
+        </>
+    );
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
+reportWebVitals();
